1052272d52fae729d92c0df4162277388ce04538
Dream-cake <thespider84@gmail.com>
2020-12-03 22:23:57 -0500

    Allowed dist

---
+++ dist/precache-manifest.b22c670ff15bd04d19b77fb630dcf192.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe5df3056bb6736788a",
+    "url": "/css/app~d0ae3f07.e1206ce2.css"
+  },
+  {
+    "revision": "5a55f6977d4c69c982b591b492dc7fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffe5df3056bb6736788a",
+    "url": "/js/app~d0ae3f07.1b57d6b2.js"
+  },
+  {
+    "revision": "ce221435051c5ef2a7cf",
+    "url": "/js/chunk-787fe830.d5ed4c87.js"
+  },
+  {
+    "revision": "7de76b634e8fa982ff78",
+    "url": "/js/chunk-vendors~0a1e6715.22211bdd.js"
+  },
+  {
+    "revision": "3c72df7d04e9576ddeb7",
+    "url": "/js/chunk-vendors~5ea1a303.d633533f.js"
+  },
+  {
+    "revision": "567fff0415aa702e3cea",
+    "url": "/js/chunk-vendors~d939e436.63c115fa.js"
+  },
+  {
+    "revision": "5f2e411c1ca4472253280fe474d94cdf",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "32f392a0d32d076f4f07393d5f687be9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4a5a950f0ee07c1804369a7b11614b12",
+    "url": "/media/fire.4a5a950f.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
